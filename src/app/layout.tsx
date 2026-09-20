6f3eb5aff1a1c66b0815d66729ab8967d785f22a
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-09-20 12:26:29 +0200

    chore: initial font setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const areal = localFont({
+  src: "../../public/font/ABCArealVariable.woff2",
+  variable: "--font-areal",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const arealMono = localFont({
+  src: "../../public/font/ABCArealMonoVariable.woff2",
+  variable: "--font-areal-mono",
 });
 
 export const metadata: Metadata = {
@@ -21,7 +21,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${areal.variable} ${arealMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
