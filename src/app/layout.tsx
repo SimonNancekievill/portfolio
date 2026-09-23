eb9854650ab93de8f7c8683a752e9d53b9a3805e
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-09-23 16:46:57 +0200

    feat: players movement added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${areal.variable} ${arealMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col my-8 mx-16">{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,47 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1 className="font-mono text-4xl">Simon Nancekievill</h1>;
+  return (
+    <>
+      <section className="">
+        <header className="pb-2">
+          <h1 className="font-areal text-sm font-medium tracking-tighter uppercase">
+            Simon Nancekievill
+          </h1>
+          <h2 className="font-mono text-xs uppercase text-gray-500">
+            Frontend Developer | Photographer |
+          </h2>
+        </header>
+        <p className="font-areal text-xs">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+          voluptates blanditiis necessitatibus aliquid quidem debitis, id
+          quisquam pariatur placeat reprehenderit ratione nemo cumque saepe
+          aperiam tempore quaerat ex ducimus eos.
+        </p>
+        <nav className="flex gap-2 my-4">
+          <Link
+            href=""
+            className="font-areal text-xs underline tracking-tight text-gray-500"
+          >
+            LinkedIn
+          </Link>
+          <Link
+            href=""
+            className="font-areal text-xs underline tracking-tight text-gray-500"
+          >
+            GitHub
+          </Link>
+          <Link
+            href=""
+            className="font-areal text-xs underline tracking-tight text-gray-500"
+          >
+            E-Mail
+          </Link>
+        </nav>
+      </section>
+      <section className="">
+        <h2 className="font-mono text-xs uppercase text-gray-500">Work</h2>
+      </section>
+    </>
+  );
 }
